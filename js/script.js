85c67ca1ac52add06ed77df1e036a3a87f3a57ff
Ashfaque Hossain Abir <ashfaquehossainabirr@gmail.com>
2025-02-04 13:09:26 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 const boxOne = document.querySelector('.box.one')
 const boxTwo = document.querySelector('.box.two')
 const boxThree = document.querySelector('.box.three')
+const boxFour = document.querySelector('.box.four')
+
+const resultBox = document.querySelector('.box.result')
 
 const submitBtnOne = document.querySelector('.submit-btn-one')
 const submitBtnTwo = document.querySelector('.submit-btn-two')
 const submitBtnThree = document.querySelector('.submit-btn-three')
+const submitBtnFour = document.querySelector('.submit-btn-four')
 
 const inputOne = document.querySelector('.inputOne')
 const inputTwo = document.querySelector('.inputTwo')
 
 const guessPlayerOne = document.querySelector('.guess-player-one')
+const guessPlayerTwo = document.querySelector('.guess-player-two')
 
 const playerOneNameDisplay = document.querySelector('.player-one-name')
+const playerTwoNameDisplay = document.querySelector('.player-two-name')
+
+const playersDisplay = document.querySelector('.players-display')
+const resultText = document.querySelector('.result-text')
+
+
+//=====================================================================
+// -- Stored Number
+//=====================================================================
+
+const storedNumber = 7
+
+
+//=====================================================================
+// -- Button Click Functionality
+//=====================================================================
 
 submitBtnOne.onclick = function () {
     if(inputOne.value == "") {
@@ -31,9 +52,50 @@ submitBtnTwo.onclick = function () {
             boxTwo.style.display = "none"
             boxThree.style.display = "flex"
 
-            console.log("Guess Number: " + guessPlayerOne.value)
+            console.log("Player 1 Guess Number: " + guessPlayerOne.value)
         } else {
             alert("Please Choose a number between 0 to 9")
         }
+    }
+}
+
+submitBtnThree.onclick = function () {
+    if(inputTwo.value == "") {
+        alert("Please Enter Your Name")
+    } else {
+        boxThree.style.display = "none"
+        boxFour.style.display = "flex"
+        playerTwoNameDisplay.innerHTML = inputTwo.value
+    }
+}
+
+submitBtnFour.onclick = function () {
+    if(guessPlayerTwo.value == "") {
+        alert("Please Enter Your Guess Number")
+    } else {
+        if(guessPlayerTwo.value >= 0 && guessPlayerTwo.value <= 9) {
+            boxFour.style.display = "none"
+            resultBox.style.display = "flex"
+
+            console.log("Player 2 Guess Number: " + guessPlayerTwo.value)
+        } else {
+            alert("Please Choose a number between 0 to 9")
+        }
+    }
+
+    if(storedNumber == guessPlayerOne.value && storedNumber !== guessPlayerTwo.value) {
+        playersDisplay.innerHTML = inputOne.value
+        resultText.innerHTML = "Congratulations you won"
+    } else if(storedNumber == guessPlayerTwo.value && storedNumber !== guessPlayerOne.value) {
+        playersDisplay.innerHTML = inputTwo.value
+        resultText.innerHTML = "Congratulations you won"
+    }  else {
+        playersDisplay.innerHTML = "Both of you"
+        resultText.innerHTML = "Game over! You all lost this game"
+    }
+
+    if(storedNumber == guessPlayerOne.value && storedNumber == guessPlayerTwo.value) {
+        playersDisplay.innerHTML = "Both of you"
+        resultText.innerHTML = "Congratulations! You all win this game"
     }
 }
